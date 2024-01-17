9ca223d963c0189e9f59c80b0c7ca12ff756c0fc
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2024-01-17 12:14:24 -0500

    update to use stream file function from bento common

--- add_md5.py
+++ add_md5.py
@@ -1,11 +1,11 @@
 import boto3
-import os
+from bento.common.utils import get_stream_md5
 import pandas as pd
 import hashlib
 
 s3_client = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='') # need to add the access key and the aws secret access key
 
-file = "test.xlsx" #the excel file name, the excel file must have file_url_in_cds
+file = "tests/test.xlsx" #the excel file name, the excel file must have file_url_in_cds
 df = pd.DataFrame()
 df = pd.read_excel(io = file, sheet_name='Metadata', engine = "openpyxl", keep_default_na = False)
 
@@ -21,8 +21,8 @@ for index, rows in df.iterrows():
         else:
             s3_file_key = s3_file_key + file_url_list[i]
     s3_file = s3_client.get_object(Bucket=s3_bucket, Key=s3_file_key)
-    s3_file_content = s3_file['Body'].read()
-    s3_hash = hashlib.md5(s3_file_content).hexdigest()
+    s3_file_content = s3_file['Body']
+    s3_hash = get_stream_md5(s3_file_content)
     print(f"The md5sum is {s3_hash}")
     df.loc[index, 'md5sum'] = s3_hash
 
